525d5b5ab9642b87298e22e13d215aab34a4fc00
Tim Mensch <tim-rz@bitgems.com>
2022-04-26 14:32:32 -0600

    Better layout
    * Wiring was really awkward before. This is cleaner.

--- keyboards/handwired/split89/split89.h
+++ keyboards/handwired/split89/split89.h
@@ -30,33 +30,23 @@
     k00, k01, k02, k03, k04, k05,       k06, k07, k08, k09, k10, k11, k12, k13, k14, k15, \
     k16, k17, k18, k19, k20, k21,       k38, k22, k23, k24, k25, k26, k27, k28, k29, k30, k31, \
     k32, k33, k34, k35, k36, k37,       k54, k55, k39, k40, k41, k42, k43, k44, k45, k46, k47, \
-    k48, k49, k50, k51, k52, k53,       k56, k57, k58, k59, k60, k61, k62,\
-    k63, k64, k65, k66, k67, k68,       k69, k70, k71, k72, k73, k74, k75, \
-    k76, k77, k78, k79, k80,            k81, k82, k83, k84, k85, k86, k87, k88 \
+    k48, k49, k50, k51, k52, k53,       k67, k68, k56, k57, k58, k59, k60, \
+    k61, k62, k63, k64, k65, k66,       k80, k69, k70, k71, k72, k73, k74,\
+    k75, k76, k77, k78, k79,            k82, k83, k84, k81, k85, k86, k87, k88 \
     ) \
     { \
       { KC_NO, KC_NO, KC_NO, KC_NO, k00, k01, k02, k03, k04, k05 }, \
       { KC_NO, KC_NO, KC_NO, KC_NO, k16, k17, k18, k19, k20, k21 },  \
       { KC_NO, KC_NO, KC_NO, KC_NO, k32, k33, k34, k35, k36, k37 },  \
       { KC_NO, KC_NO, KC_NO, KC_NO, k48, k49, k50, k51, k52, k53 },  \
-      { KC_NO, KC_NO, KC_NO, KC_NO, k63, k64, k65, k66, k67, k68 },  \
-      { KC_NO, KC_NO, KC_NO, KC_NO, k76, k78, k79, k77, KC_NO, k80 },  \
+      { KC_NO, KC_NO, KC_NO, KC_NO, k61, k62, k63, k64, k65, k66 },  \
+      { KC_NO, KC_NO, KC_NO, KC_NO, k75, k77, k78, k76, KC_NO, k79 },  \
       { k06, k07, k08, k09, k10, k11, k12, k13, k14, k15 }, \
       { k22, k23, k24, k25, k26, k27, k28, k29, k30, k31 },  \
       { k38, k39, k40, k41, k42, k43, k44, k45, k46, k47 },  \
-      { k54, k55, k56, k57, k58, k59, k60, k61, k62, KC_NO },  \
-      { k69, k70, KC_NO, k71, k72, k73, k74, KC_NO, k75, KC_NO },  \
-      { k81, KC_NO, KC_NO, k82, k83, k84, k85, k86, k87, k88 }  \
-}
+      { k54, k55, k56, k57, k58, k59, k60, KC_NO, KC_NO, KC_NO },  \
+      { k67, k68, k69, k70, k71, k72, k73, KC_NO, k74, KC_NO },  \
+      { k80, KC_NO, k81, k82, k83, k84, k85, k86, k87, k88 }  \
+    }
 
-/*
-KC_SPC, k82
-KC_RALT, k83
-MO(1), k84
-MO(1), k85
-KC_RCTL, k86
-KC_LEFT, k87
-KC_DOWN, k88
-KC_RGHT, k89
-*/
 
