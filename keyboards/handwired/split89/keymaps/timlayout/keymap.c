2f2db607bbd6e50e3ed707a144b044893a3dd9d5
Tim Mensch <tim-rz@bitgems.com>
2026-03-11 01:18:37 -0600

    Updated keymap

--- keyboards/handwired/split89/keymaps/timlayout/keymap.c
+++ keyboards/handwired/split89/keymaps/timlayout/keymap.c
@@ -28,16 +28,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,            KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,  KC_INS, KC_HOME, KC_PGUP,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,            KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, KC_END, KC_PGDN,
         MO(1), KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,           KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,                    KC_UP,
-        KC_LCTL, MO(1), KC_LGUI, KC_LALT, KC_SPC,        KC_SPC, KC_RALT, MO(1), MO(1), KC_RCTL,                  KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V,  KC_B,           KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,                    KC_UP,
+        KC_LCTL, MO(1), KC_LGUI, KC_LALT, LT(2,KC_SPC),        LT(2,KC_SPC), KC_RALT, MO(1), MO(1), KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT
     ),
 	[1] = LAYOUT(
         KC_SLEP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                        KC_NO, KC_NO, KC_SLEP,
         KC_MPRV, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WBAK,               KC_NO, KC_NO, KC_WBAK,
-        KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MEDIA_PLAY_PAUSE,   KC_SLEP, KC_STOP, KC_WFWD,
+        KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MEDIA_PLAY_PAUSE,    KC_SLEP, KC_STOP, KC_WFWD,
         KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_CALC, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_CALC,                                      KC_VOLU,
-        KC_NO, KC_TRNS, KC_NO, KC_NO, KC_MEDIA_PLAY_PAUSE,  KC_MEDIA_PLAY_PAUSE, KC_MENU, KC_TRNS, KC_TRNS, KC_RGUI,                              KC_MRWD, KC_VOLD, KC_MFFD
+        KC_NO, KC_TRNS, KC_NO, KC_NO, KC_MEDIA_PLAY_PAUSE,  KC_MEDIA_PLAY_PAUSE, KC_MENU, KC_TRNS, KC_TRNS, KC_RGUI,                KC_MPRV, KC_VOLD, KC_MNXT
+    ),
+    // Reverse Qwerty layer
+    [2] = LAYOUT(
+        KC_BSPC, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO,     KC_ESC, KC_NO, KC_F5, KC_F1, KC_F2, KC_F3, KC_F4,                   KC_NO, KC_NO, KC_SLEP,
+        KC_MINS, KC_0, KC_9, KC_8, KC_7, KC_6,             KC_5, KC_4, KC_3, KC_2, KC_1, KC_GRV, KC_NO, KC_WBAK,               KC_NO, KC_NO, KC_WBAK,
+        KC_EQL, KC_P, KC_O, KC_I, KC_U, KC_Y,              KC_T, KC_R, KC_E, KC_W, KC_Q, KC_TAB, KC_TAB, KC_TAB,               KC_SLEP, KC_STOP, KC_WFWD,
+        KC_QUOT, KC_SCLN, KC_L, KC_K, KC_J, KC_H,          KC_G, KC_F, KC_D, KC_S, KC_A, KC_NO, KC_NO,
+        KC_RSFT, KC_NO, KC_NO, KC_CALC, KC_NO, LT(3,KC_N), KC_B, KC_V, KC_C, KC_X, KC_Z, KC_LSFT,                                       KC_NO,
+        KC_LCTL, KC_TRNS, KC_ENT, KC_NO, KC_TRNS,          KC_TRNS, KC_MENU, KC_TRNS, KC_TRNS, KC_RGUI,                        KC_NO,   KC_NO, KC_NO
+    ),
+    // Reverse Qwerty supplemental layer
+    [3] = LAYOUT(
+        KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,              KC_NO, KC_NO, KC_SLEP,
+        KC_NO, KC_NO, KC_NO, KC_INS, KC_HOME, KC_PGUP,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_WBAK,
+        KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, KC_END, KC_PGDN,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       KC_SLEP, KC_STOP, KC_WFWD,
+        KC_TRNS, KC_NO, KC_UP, KC_LEFT, KC_DOWN, KC_RGHT,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_TRNS,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                            KC_NO,
+        KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS,               KC_TRNS, KC_MENU, KC_TRNS, KC_TRNS, KC_RGUI,                  KC_NO, KC_NO, KC_NO
     )
 };
 
